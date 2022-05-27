8ccc81d5cac0fb02bfd5a37d87fdee7c492158cc
sangepu-pradeep <106117363+sangepu-pradeep@users.noreply.github.com>
2022-05-27 11:47:19 +0530

    Time: 5 ms (14.10%), Space: 6 MB (26.66%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    int fun(int num,vector<int>&dp){
-    if(dp[num]!=-1) return dp[num];
-        if(num==0) return 0;
-        int ans=INT_MAX;
-        if(num%2==0)ans=1+fun(num/2,dp);
-        ans=min(ans,1+fun(num-1,dp));
-        return dp[num]=ans;
-    }
+    
     int numberOfSteps(int num) {
-        vector<int>dp(num+1,-1);
-        return fun(num,dp);
+        int cnt=0;
+        while(num>0){
+           if((num&1)) num--;
+            else num=(num>>1);
+            cnt++;
+        }
+        return cnt;
     }
 };
