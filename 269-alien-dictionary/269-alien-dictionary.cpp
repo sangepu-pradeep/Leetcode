af84c0ae8b9ded4877b2990031bf829d3589af7a
sangepu-pradeep <106117363+sangepu-pradeep@users.noreply.github.com>
2022-05-28 08:01:05 +0530

    Time: 16 ms (7.52%), Space: 9.9 MB (40.30%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.cpp
+++ 269-alien-dictionary/269-alien-dictionary.cpp
@@ -1,65 +1,52 @@
 class Solution {
 public:
-    void fun(string &a, string &b, vector<vector<int>>&g){
-        int i=0;int j=0;
-        while(i<a.size()&&j<b.size()){
-            if(a[i]==b[j]){            
-                i++;j++;
-            }else {
-                g[a[i]-'a'].push_back(b[j]-'a');            
-                break;
-            }            
-        }
-        if(j==b.size()&&i<a.size()){
-            g[a[0]-'a'].push_back(a[0]-'a');
-        }
-        
-    }
-    bool cycle(vector<int>&vis,vector<int>&curr,vector<vector<int>>&g, int node,stack<int>&st){
-        vis[node]=1;
-        curr[node]=1;
-        for(auto a:g[node]){         
-          
-            if(vis[a]==0){
-                if(cycle(vis,curr,g,a,st)) return 1;
-            }
-             else if(curr[a]==1) return 1;
-        }
-        curr[node]=0;
-        st.push(node);
-        return 0;
-    }
 
-    string alienOrder(vector<string>& w) {
-      vector<int>y(26);
-        for(auto a:w){
-            for(auto b:a){
-                y[b-'a']=1;
-            
-            }
+    map<char,bool> vis; map<char,bool>curr; stack<char>st;
+    bool dfs(map<char,vector<char>>&g,char node){
+        vis[node]=true;curr[node]=true;
+        for(auto it:g[node]){
+            if(!vis[it]&&dfs(g,it)) return true;
+            else if(curr[it]) return true;
         }
-        vector<vector<int>>g(26);
-        for(int i=0;i<w.size();i++){
-            for(int j=i+1;j<w.size();j++){
-               fun(w[i],w[j],g);
-            }
-        }
-        stack<int>st; 
-       vector<int>curr(26); vector<int>vis(26);
-        for(int i=0;i<26;i++){
-            if(vis[i]==0&&y[i]==1&&cycle(vis,curr,g,i,st)){
-              { return {};}
-            }
-        }
-       string ans;
-        while(st.size()){
-            int c=st.top(); st.pop();
-            ans.push_back(c+'a');
-        }
+        curr[node]=false;
+        st.push(node);
+        return false;
         
-     for(int i=0;i<26;i++){ 
-         if(vis[i]==0&&y[i]==1) {ans.push_back(i+'a');}
-     }
+    }
+    string alienOrder(vector<string>& words) {
+            map<char,vector<char>> g;
+        for(auto i:words){
+            for(auto j:i){
+               g.insert({j,{}});
+            }
+        }
+       
+       for(int i=0;i<words.size()-1;i++){
+          string word1=words[i]; string word2=words[i+1]; 
+           if(word1.size()>word2.size()&&word1.find(word2)==0){
+               return "";
+           }
+           for(int j=0;j<min(word1.size(),word2.size());j++){
+               if(word1[j]!=word2[j]){
+                   g[word1[j]].push_back(word2[j]); break;
+               }
+           }
+               
+       }
+       curr.clear(); vis.clear();
+        for(auto node:g){
+            vis.insert({node.first,false});
+            curr.insert({node.first,false});
+        }
+        st={};
+        for(auto node:g){
+            if(!vis[node.first]&&dfs(g,node.first)) return "";
+        }
+         string ans; 
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
         return ans;
     }
 };
