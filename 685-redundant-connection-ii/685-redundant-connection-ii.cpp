2314701ed46a094400501242f40bb61c33d98e25
sangepu-pradeep <106117363+sangepu-pradeep@users.noreply.github.com>
2022-05-28 14:51:29 +0530

    Time: 14 ms (48.87%), Space: 9.5 MB (73.07%) - LeetHub

--- 685-redundant-connection-ii/685-redundant-connection-ii.cpp
+++ 685-redundant-connection-ii/685-redundant-connection-ii.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
-    bool fun(int node,int &n,vector<int>&vis,vector<vector<int>>&g, int s, int e){
-        vis[node]=1;n--;
-        if(n==0) return 1;
-        for(auto a:g[node]){
-            if(node==s&&a==e)continue;
-           
-            if(vis[a]==0&&fun(a,n,vis,g,s,e)) return 1;
-        }
-        
-        return 0;
-    }
-    vector<int> findRedundantDirectedConnection(vector<vector<int>>& e) {
-        int n=e.size();
-        vector<int>in(n+1); vector<vector<int>>g(n+1);
-       for(auto a:e){
-           in[a[1]]++; g[a[0]].push_back(a[1]);
-       }
-        vector<int>vis(n+1);
-        reverse(e.begin(),e.end());
-        for(auto a:e){
-            in[a[1]]--;;
-            for(int i=1;i<=n;i++){
-                int p=n; 
-                if(in[i]==0) {
-                  int b=fun(i,p,vis,g,a[0],a[1]);
-               
-                    if(b==1) return a;
-                   for(int i=1;i<=n;i++)vis[i]=0;
-                    break;
-                }
-               
+    
+ vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int>parent(n+1,0),V1,V2;
+     for(auto &edge:edges){
+         if(parent[edge[1]]==0){
+           parent[edge[1]]=edge[0];  
+         }
+         else{
+             V1={parent[edge[1]],edge[1]};
+            V2=edge; 
+             edge[1]=0;
+         }
+     }
+    
+     for(int i=1;i<=n;i++){
+        parent[i]=i; 
+     }
+ 
+     for(auto &edge:edges){
+         if(edge[1]==0) continue;
+         
+         int u = edge[0], v = edge[1], pu = root(parent, u);
+            // Now every node only has 1 parent, so root of v is implicitly v
+            if (pu == v) {
+                if (V1.empty()) return edge;
+                return V1;
             }
-            in[a[1]]++;
+            parent[v] = pu;
         }
-        return {};
+        return V2;
+     }
+  
+     int root(vector<int> &parent, int k){
+        
+        if(parent[k]!=k) 
+           
+         parent[k]=root(parent,parent[k]);
+            return parent[k];
     }
 };
