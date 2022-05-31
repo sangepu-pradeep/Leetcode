f9c7b1189383088839100443882c9ed2195bad9f
sangepu-pradeep <106117363+sangepu-pradeep@users.noreply.github.com>
2022-05-31 10:07:50 +0530

    Time: 91 ms (50.39%), Space: 29.1 MB (28.71%) - LeetHub

---
+++ 1136-parallel-courses/1136-parallel-courses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minimumSemesters(int n, vector<vector<int>>& r) {
+        vector<vector<int>>g(n); vector<int>in(n);
+        for(auto a:r){
+            --a[0];--a[1];
+           g[a[0]].push_back(a[1]);
+            in[a[1]]++;
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(in[i]==0)q.push(i);
+        }
+        int cnt=0; int ans=0;
+        while(q.size()){
+            int sz=q.size();
+            int s=q.size();
+           while(s--) {
+               int a=q.front();
+            q.pop();
+            for(auto t:g[a]){
+                in[t]--;
+                if(in[t]<0) return -1;
+                if(in[t]==0)q.push(t);
+            }
+           }
+            cnt+=sz;
+            ans++;
+        }
+       // cout<<cnt<<" "<<n;
+        if(cnt==n) return ans;
+     
+    return -1;
+    }
+};
