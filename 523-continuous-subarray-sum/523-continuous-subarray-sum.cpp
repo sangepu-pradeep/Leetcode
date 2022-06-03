37c08f8a9e17cd63ff643a721c5dc58b6cd15cf1
sangepu-pradeep <106117363+sangepu-pradeep@users.noreply.github.com>
2022-06-03 10:26:47 +0530

    Time: 207 ms (76.44%), Space: 100.5 MB (6.08%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
@@ -10,14 +10,11 @@ public:
             prefix[i]=nums[i-1]+prefix[i-1];
             int p=prefix[i]%k;
            
-            if(mp.count(p)){
-               
-                int t=mp[p];
-                // cout<<i<<" "<<t<<endl;
-                if(i-t>1){return 1;}
-               // else if(t==0&&i-t>1) return 1;
+            if(mp.count(p)){            
+                int t=mp[p];         
+                if(i-t>1){return 1;}            
             }
-           if(mp.count(p)==0) mp[p]=i;
+           if(mp.find(p)==mp.end()) mp[p]=i;
         }
         return 0;
             
