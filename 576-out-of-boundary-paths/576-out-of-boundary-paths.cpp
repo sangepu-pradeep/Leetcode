f7358c19b2574e90caf2b56965f04cf5f4f8bdf4
sangepu-pradeep <106117363+sangepu-pradeep@users.noreply.github.com>
2022-07-16 22:26:52 +0530

    Time: 9 ms (79.68%), Space: 6.4 MB (90.53%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int dirx[4]={1,-1,0,0};
+    int diry[4]={0,0,-1,1};
+    bool valid(int x, int y, int r, int c){
+        return x>=0&&x<r&&y>=0&&y<c;
+    }
+    int mod=1e9+7;
+     int dp[51][51][51];
+    int fun(int x, int y, int m, int r, int c){
+        if(dp[x][y][m]!=-1) return dp[x][y][m];
+       if(m==1){
+           int l=0;
+            if(x==0)  l++;
+           if(x==r-1) l++;
+           if(y==0)l++;
+           if(y==c-1)l++;
+          return l;
+       }
+   int ans=0;
+        for(int i=0;i<4;i++){
+           int x1=x+dirx[i]; int y1=y+diry[i];
+            if(valid(x1,y1,r,c)) ans+=fun(x1,y1,m-1,r,c);
+            else ans++;
+            ans=ans%mod;
+        }
+        return dp[x][y][m]=ans;
+    }
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        memset(dp,-1,sizeof(dp));
+        if(maxMove==0) return 0;
+        return fun(startRow,startColumn,maxMove,m,n);
+    }
+};
