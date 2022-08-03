86f8de668a734cc4dabeb8d5d4604fb0de6f335c
sangepu-pradeep <106117363+sangepu-pradeep@users.noreply.github.com>
2022-08-03 18:47:12 +0530

    Time: 88 ms (12.08%), Space: 13.9 MB (9.32%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    bool solve(unordered_map<char,int>&vis){
-        for(auto a:vis){
-            if(a.second>1) return 0;
-        }
-        return 1;
-    }
-    bool fun(int k, string &s){
-   unordered_map<char,int>vis;
+    bool valid(int k, string &s){
+       unordered_map<char,int>vis;
         for(int i=0;i<k;i++){
             vis[s[i]]++;
         }
-        if(solve(vis)) return 1;
+        bool b=0;
+       // for(int i=0;i<26;i++) if(vis[i]>1){b=1;break;}
+        for(auto a:vis)if(a.second>1){b=1;break;}
+        if(b==0) return 1;
         for(int i=k;i<s.size();i++){
+            vis[s[i]]++;
             vis[s[i-k]]--;
-              vis[s[i]]++;
-                if(solve(vis)) return 1;
+            bool b=0;
+            for(auto a:vis)if(a.second>1){b=1;break;}
+        if(b==0) return 1;
         }
         return 0;
     }
     int lengthOfLongestSubstring(string s) {
 
-        if(s=="") return 0;
-        int l=1; int h=s.size(); int ans=1;
-     //   return fun(2,s);
-        while(l<=h){ 
+       int l=0; int h=s.size(); int ans=1;
+        
+        while(l<=h){
             int mid=(l+h)/2;
-            if(fun(mid,s)) {l=mid+1;ans=mid;}
+            if(valid(mid,s)){ l=mid+1; ans=mid;}
             else h=mid-1;
         }
         return ans;
