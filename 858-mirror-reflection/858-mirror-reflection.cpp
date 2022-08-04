8b6e1b7ea7d365588223a84d076f70530a66b3f0
sangepu-pradeep <106117363+sangepu-pradeep@users.noreply.github.com>
2022-08-05 02:30:14 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.45%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mirrorReflection(int p, int q) {
+        int t=p/q;
+       while(p%2==0&&q%2==0){
+           p/=2;q/=2;
+       }
+        if(p%2&&q%2) return 1;
+        if(p%2&&q%2==0) return 0;
+        return 2;
+    }
+};
