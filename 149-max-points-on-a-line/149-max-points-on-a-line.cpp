79ec2cb2de21280537e27c9fa98df88aefe08a2f
sangepu-pradeep <106117363+sangepu-pradeep@users.noreply.github.com>
2022-08-19 13:47:27 -0700

    Time: 86 ms (41.49%), Space: 26.5 MB (7.68%) - LeetHub

---
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+   
+    int maxPoints(vector<vector<int>>& p) {
+        int ans=1;
+        long double r=1e9;
+        for(int i=0;i<p.size();i++){
+            unordered_map<long double,int>mp;
+            for(int j=0;j<p.size();j++){
+                if(i==j)continue;
+                 long double t;
+                if( (long double)p[j][0]- (long double)p[i][0]==0)t=1e9;
+               else t=( (long double)p[j][1]- (long double)p[i][1])/( (long double)p[j][0]- (long double)p[i][0]);
+                mp[t]++;               
+            }
+            for(auto a:mp){
+                ans=max(ans,a.second+1);
+            }
+        }
+        return ans;
+    }
+};
