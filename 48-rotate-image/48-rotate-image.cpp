549bbc17bdb1d105bbde6b0b300ad56393d85fde
sangepu-pradeep <106117363+sangepu-pradeep@users.noreply.github.com>
2022-08-29 22:06:05 -0700

    Time: 0 ms (100.00%), Space: 7.1 MB (73.79%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -8,14 +8,14 @@ public:
             int i=0;int f=0;int p=1;
         int l=0; int h=r-1;
         while(i<r/2){
-            for( int j=l;j<h;j++){
+            for( int j=f;j<r-f-1;j++){
                 int t=m[i][j];
                 m[i][j]=m[r-j-1][i];// 
                 m[r-j-1][i]=m[r-i-1][r-j-1];
                 m[r-i-1][r-j-1]=m[j][r-i-1];
                 m[j][r-i-1]=t;
             }
-            i++;l++;h--;
+            i++;l++;h--;f++;
         }
         return ;
        
