d4a93f4a62776aa6aff7f86e83b8efe0e31f4e42
sangepu-pradeep <106117363+sangepu-pradeep@users.noreply.github.com>
2022-08-29 22:04:47 -0700

    Time: 6 ms (23.35%), Space: 7 MB (73.79%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& m) {
+      // i, j-> j,r-i-1
+        // r-i-1, r-j-1
+        //     r-j-1, i
+        int r=m.size(); int c=m[0].size();
+            int i=0;int f=0;int p=1;
+        int l=0; int h=r-1;
+        while(i<r/2){
+            for( int j=l;j<h;j++){
+                int t=m[i][j];
+                m[i][j]=m[r-j-1][i];// 
+                m[r-j-1][i]=m[r-i-1][r-j-1];
+                m[r-i-1][r-j-1]=m[j][r-i-1];
+                m[j][r-i-1]=t;
+            }
+            i++;l++;h--;
+        }
+        return ;
+       
+    }
+};
