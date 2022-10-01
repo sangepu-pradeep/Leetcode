484a430a8d9b1b83c10339aaad3462ee3b6028e3
sangepu-pradeep <106117363+sangepu-pradeep@users.noreply.github.com>
2022-10-01 14:17:07 -0700

    Time: 0 ms (100.00%), Space: 5.9 MB (81.42%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     double fun(double x, int n){
-        if(n==0) return (double)1;
-        if(n%2==0) {
-            double t=fun(x,n/2);
+        if(n==0) return 1;
+        if(n==1) return x;
+        if(n==-1) return (1/x);
+         double t=fun(x,n/2);
+        if(n%2==0){
+           
             return t*t;
         }
-        return x*fun(x,n-1);
+        if(n<0) return (1/x)*t*t;
+        return x*t*t;
     }
     double myPow(double x, int n) {
-        if(n==0) return 1;
-        if(n==INT_MIN){
-            return (1.0/x)*myPow(x,n+1);
-        }
-        if(n<0) return 1.0/fun(x,-n);
-        return fun(x,n);
+         return fun(x,n);
     }
 };
