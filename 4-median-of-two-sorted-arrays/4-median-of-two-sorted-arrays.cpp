cdf284540bc86e1749860d9ab80595b447e58ef1
sangepu-pradeep <106117363+sangepu-pradeep@users.noreply.github.com>
2022-10-01 14:09:53 -0700

    Time: 60 ms (63.95%), Space: 89.3 MB (64.69%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
     int fun(vector<int>&nums1,vector<int>&nums2, int k){
-        int l=INT_MAX; int h=INT_MIN;
-        if(nums1.size()){
-            l=min(l,nums1[0]);
-            h=max(h,nums1.back());
-        }
-        if(nums2.size()){
-            l=min(l,nums2[0]);
-            h=max(h,nums2.back());
-        }
+        
+        
+        int l=min(nums1.size()?nums1[0]:INT_MAX,nums2.size()?nums2[0]:INT_MAX);
+        int h=max(nums1.size()?nums1.back():INT_MIN,nums2.size()?nums2.back():INT_MIN);
         int ans=0;
         while(l<=h){
             int mid=(l+h)/2;
-            int p=upper_bound(nums1.begin(),nums1.end(),mid)-nums1.begin();
-            int q=upper_bound(nums2.begin(),nums2.end(),mid)-nums2.begin();
-            if(p+q>k){
-                h=mid-1;ans=mid;
-            }else{
-                l=mid+1;//ans=mid;
+            int l1=upper_bound(nums1.begin(),nums1.end(),mid)-nums1.begin();
+          int l2=upper_bound(nums2.begin(),nums2.end(),mid)-nums2.begin(); 
+            if(l1+l2<=k){
+             l=mid+1;
             }
-            
+            else    {ans=mid; h=mid-1;   }
         }
         return ans;
     }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-         int n1=nums1.size(); int n2=nums2.size();
-        double ans=0;
-        if((n1+n2)%2==0){
-         ans=(double)(fun(nums1,nums2,(n1+n2)/2-1)+fun(nums1,nums2,(n1+n2)/2));
-            //cout<<ans<<endl;
-            ans=ans/2;
+       int l=nums1.size()+nums2.size();
+        if(l%2){
+         return fun(nums1,nums2,l/2);
             
-        }else {
-            ans=fun(nums1,nums2,(n1+n2)/2);
         }
-        return ans;
+       double ans= fun(nums1,nums2,l/2-1)+fun(nums1,nums2,l/2);
+        ans/=2;
+         return ans;
     }
 };
